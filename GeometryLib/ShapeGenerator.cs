cc14e318dd05cf601ddb4b48324519860723e44d
Aktis Enne <944kvd@gmail.com>
2023-02-07 15:16:13 +0300

    feat: shape generator

---
+++ GeometryLib/ShapeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using GeometryLib.Shapes;
+
+namespace GeometryLib;
+
+public static class ShapeGenerator
+{
+    public static IShape GenerateShape(List<double> sides) =>
+        sides.Count switch
+        {
+            1 => new Circle(sides[0]),
+            3 => new Triangle(sides[0], sides[1], sides[2]),
+            _ => throw new ArgumentOutOfRangeException(nameof(sides), "Incompatible number of sides")
+        };
+}
